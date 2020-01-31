49d5207b505da3ad213f46631f9cad441c3d2efa
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2020-01-31 10:38:20 -0700

    updating client

---
+++ petful-client/src/Routes/Adoption.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Adoption.css";
+
+export default class Adoption extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      dogs: [],
+      cats: []
+    };
+  }
+  componentDidMount() {
+    this.getDogs();
+    this.getCats();
+  }
+
+  getDogs = () => {};
+
+  getCats = () => {};
+
+  render() {
+    return (
+      <div className="Adoption">
+        <h1>Adoption Process</h1>
+        <div className="dog">
+          <h2>Dogs</h2>
+          dog info here
+        </div>
+
+        <div className="cat">
+          <h2>Cats</h2>
+          cat info here
+        </div>
+
+        <div>
+          <h3>Place in line</h3>
+          queue info goes here
+        </div>
+      </div>
+    );
+  }
+}
