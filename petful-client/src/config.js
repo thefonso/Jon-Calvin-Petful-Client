6377951927cbdd609d87ea34857141a198e85ba7
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2020-02-20 20:12:46 -0700

    jons head is detached, working on this version

--- petful-client/src/config.js
+++ petful-client/src/config.js
@@ -1,5 +1,5 @@
 export default {
-  REACT_APP_API_BASE: 'https://jon-calvin-petful.herokuapp.com/' || 'http://localhost:8080/api'
+  REACT_APP_API_BASE: "http://localhost:8080"
   //'https://jon-calvin-petful.herokuapp.com/api' ||
   //process.env.REACT_APP_API_BASE "
   // https://jon-calvin-petful.herokuapp.com/
