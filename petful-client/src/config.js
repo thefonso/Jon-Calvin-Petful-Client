51b28eb04f0f5a38408274f9890f353d2cbe439b
Jon <jonathanjackson424@gmail.com>
2020-02-20 19:49:44 -0500

    took out /api in config

--- petful-client/src/config.js
+++ petful-client/src/config.js
@@ -1,4 +1,6 @@
 export default {
-  REACT_APP_API_BASE: 'https://jon-calvin-petful.herokuapp.com/api' || 'http://localhost:8080/api'
+  REACT_APP_API_BASE: 'https://jon-calvin-petful.herokuapp.com/' || 'http://localhost:8080/api'
+  //'https://jon-calvin-petful.herokuapp.com/api' ||
   //process.env.REACT_APP_API_BASE "
+  // https://jon-calvin-petful.herokuapp.com/
 };
